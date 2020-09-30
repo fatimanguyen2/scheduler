65b9d576f0164c90285aa4027cb13446ec93d595
Fatima Nguyen <fatima3455@hotmail.com>
2020-09-30 14:53:58 -0400

    install axios and obtain days arr from api request

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 import "components/Application.scss";
 import DayList from 'components/DayList';
 import Appointment from 'components/Appointments'
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = [
   {
@@ -70,7 +53,18 @@ const appointments = [
 ];
 
 export default function Application(props) {
+  const [days, setDays] = useState([]);
   const [day, setDay] = useState('Monday');
+
+  useEffect(() => {
+    axios.get('/api/days')
+      .then( res => setDays(res.data))
+      .catch(err => {
+        console.log(err.response.status)
+        console.log(err.response.headers)
+        console.log(err.response.data)
+      })
+  }, []);
   return (
     <main className="layout">
       <section className="sidebar">
