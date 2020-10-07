6062329ac31f0d9e60872dcc1ef75162f90740c9
Fatima Nguyen <fatima3455@hotmail.com>
2020-10-07 16:15:05 -0400

    handle no interviewer selected error

--- src/components/Appointments/Form.jsx
+++ src/components/Appointments/Form.jsx
@@ -5,7 +5,8 @@ import InterviewerList from 'components/InterviewerList';
 export default function Form(props) {
   const [name, setName] = useState(props.name || '');
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
-  const [error, setError] = useState("");
+  const [studentError, setStudentError] = useState("");
+  const [interviewerError, setInterviewerError] = useState("");
 
   const reset = () => {
     setName('');
@@ -17,18 +18,23 @@ export default function Form(props) {
   };
 
   const validate = () => {
-    if (name === "") {
-      setError("Student name cannot be blank");
+    if (!name) {
+      setStudentError("Student name cannot be blank. ");
       return;
     }
-    setError('')
+    if (!interviewer) {
+      setInterviewerError("Interviewer must be selected.");
+      return;
+    }
+    setStudentError('');
+    setInterviewerError('');
     props.onSave(name, interviewer);
   }
 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off" onSubmit = {event => event.preventDefault()}>
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name='name'
@@ -39,7 +45,10 @@ export default function Form(props) {
             data-testid="student-name-input"
           />
         </form>
-        <section className="appointment__validation">{error}</section>
+        <section className="appointment__validation">
+          {studentError}
+          {interviewerError}
+        </section>
         <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
       </section>
       <section className="appointment__card-right">
